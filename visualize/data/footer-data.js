9545bcc30a92de152e76fa26e67cba7eeb90214d
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-04-30 18:59:04 +0800

    Add assets

---
+++ visualize/data/footer-data.js
@@ -0,0 +1,38 @@
+var currentDate = new Date()
+
+footer_data = {
+  "copyright" : {
+    "organization" : {
+      "link" : "http:\/\/teelab.net\/",
+      "name" : "TEELAB"
+    },
+    "year" : currentDate.getFullYear()
+  },
+  "powered_by" : [
+    {
+      "version" : "",
+      "link" : "http:\/\/github.io\/",
+      "name" : "GitHub Pages"
+    },
+    {
+      "version" : "v3.3.6",
+      "link" : "http:\/\/getbootstrap.com\/",
+      "name" : "Bootstrap"
+    },
+    {
+      "version" : "v1.11.3",
+      "link" : "http:\/\/jquery.com\/",
+      "name" : "jQuery"
+    },
+    {
+      "version" : "v4.0.0",
+      "link" : "http:\/\/necolas.github.io\/normalize.css\/",
+      "name" : "normalize.css"
+    },
+    {
+      "version" : "v4.6.1",
+      "link" : "http:\/\/fortawesome.github.io\/Font-Awesome\/",
+      "name" : "Font Awesome"
+    }
+  ]
+};
