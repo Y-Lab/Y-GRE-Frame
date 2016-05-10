3235d8caf9b1fa1a89f68f9e843484f6542a9f97
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-05-11 01:20:32 +0800

    Update

--- visualize/set-framework.js
+++ visualize/set-framework.js
@@ -1,4 +1,12 @@
 // set-framework.js
 
+// Title
 document.title = frame_data.general.basics.name + ' · Y-English GRE Test Preparation Framework';
 $('#site-version').html(footer_data.site.version + ' for ' + frame_data.general.basics.name);
+
+// General
+$('#main-container').append($('<div>').attr('id', 'general-div').addClass('bs-docs-section')
+    .append($('<h1>').attr('id', 'general-h1').addClass('page-header').html('General'))
+    .append($('<h3>').attr('id', 'general-basics-h3').html('Basics'))
+    .append($('<div>').attr('id', 'general-basics-div'))
+);
