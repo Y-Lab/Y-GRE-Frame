9a9eae8a4bfde2a5d2ea6db421ad9728cd34e71c
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-05-10 20:54:44 +0800

    Update

--- visualize/set-framework.js
+++ visualize/set-framework.js
@@ -1,6 +1,14 @@
 // set-framework.js
 
+function enterPasskey(e) {
+    var e = e || window.event;
+    if (e.keyCode == 13) {
+        var passkey = $('#input-passkey').val();
+        window.location.href = 'http://frame.y-gre.org/framework/?passkey=' + passkey;
+    };
+};
+
 function submitPasskey() {
     var passkey = $('#input-passkey').val();
     window.location.href = 'http://frame.y-gre.org/framework/?passkey=' + passkey;
-}
+};
