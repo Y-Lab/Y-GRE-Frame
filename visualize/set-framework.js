3067297eaa6a2bab24164f529561d945e2a9d43c
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-05-10 23:07:30 +0800

    Update

--- visualize/set-framework.js
+++ visualize/set-framework.js
@@ -13,26 +13,26 @@
 //     window.location.href = 'http://frame.y-gre.org/framework/?passkey=' + passkey;
 // };
 
-function urlJump() {
-    var passkey = $('#input-passkey').val();
-    window.location.href = 'http://frame.y-gre.org/framework/?passkey=' + passkey;
-};
+// function urlJump() {
+//     var passkey = $('#input-passkey').val();
+//     window.location.href = 'http://frame.y-gre.org/framework/?passkey=' + passkey;
+// };
 
 
-$('#submit-passkey').click(
-    function() {
-        urlJump();
-    }
-);
+// $('#submit-passkey').click(
+//     function() {
+//         urlJump();
+//     }
+// );
 
-$('#input-passkey').keydown(
-    function(event) {
-        if (event.keyCode == '13') {
-            urlJump();
-            // $('#submit-passkey').click()
-        }
-    }
-);
+// $('#input-passkey').keydown(
+//     function(event) {
+//         if (event.keyCode == '13') {
+//             urlJump();
+//             // $('#submit-passkey').click()
+//         }
+//     }
+// );
 
 
 // $(function(){
