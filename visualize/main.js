4cb6dac205b7148d9dac8dfcd9d739af6fae7f3e
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-05-11 00:40:21 +0800

    Update

--- visualize/main.js
+++ visualize/main.js
@@ -14,61 +14,52 @@ function getQueryVariable(variable) {
 
 passkey = getQueryVariable('passkey');
 
-if (passkey) {
-    data_file = 'data/' + passkey;
-    visualize_file = 'set-framework';
+var data_file = 'data/' + passkey;
+var visualize_file = 'set-framework';
+var default_file = 'set-framework-default';
 
-    require.config({
-        paths: {
-            'data': data_file,
-            'visualize': visualize_file,
-        }
-    });
+require.config({
+    paths: {
+        'data': data_file,
+        'visualize': visualize_file,
+        'default': default_file,
+    }
+});
 
-    $('#loading-progress-bar div span').html('Loading data...');
-    require(
-        ['data'],
-        function (data) {
-            $('#loading-progress-bar div span').html('Rendering page... (｡•ˇ‸ˇ•｡)');
-            require(
-                ['visualize'],
-                function (visualize) {
-                    $('#loading-progress-bar div').removeClass('progress-bar-striped');
-                    $('#loading-progress-bar div span').html('Well done! ~\(≧▽≦)/~');
-                    $('#loading-progress-bar').delay(4000).fadeOut(1000, function() {$(this).remove()});
-                },
-                function (err) {
-                    $('#loading-progress-bar div').removeClass('progress-bar-striped').removeClass('progress-bar-blue').addClass('progress-bar-danger');
-                    $('#loading-progress-bar div span').html('Unable to load component: ' + visualize_file + '.js');
-                }
-            );
-        },
-        function (err) {
-            $('#loading-progress-bar div').removeClass('progress-bar-striped').removeClass('progress-bar-blue').addClass('progress-bar-danger');
-            $('#loading-progress-bar div span').html('Unable to load component: ' + data_file + '.js');
-        }
-    );
-}
-else {
-    visualize_file = 'set-framework-empty';
-
-    require.config({
-        paths: {
-            'visualize': visualize_file,
-        }
-    });
-
-    $('#loading-progress-bar div span').html('Rendering page... (｡•ˇ‸ˇ•｡)');
-    require(
-        ['visualize'],
-        function (visualize) {
-            $('#loading-progress-bar div').removeClass('progress-bar-striped');
-            $('#loading-progress-bar div span').html('Well done! ~\(≧▽≦)/~');
-            $('#loading-progress-bar').delay(4000).fadeOut(1000, function() {$(this).remove()});
-        },
-        function (err) {
-            $('#loading-progress-bar div').removeClass('progress-bar-striped').removeClass('progress-bar-blue').addClass('progress-bar-danger');
-            $('#loading-progress-bar div span').html('Unable to load component: ' + visualize_file + '.js');
-        }
-    );
-};
+$('#loading-progress-bar div span').html('Loading data...');
+require(
+    ['data'],
+    function (data) {
+        $('#loading-progress-bar div span').html('Busy rendering page... (｡•ˇ‸ˇ•｡)');
+        require(
+            ['visualize'],
+            function (visualize) {
+                $('#loading-progress-bar div').removeClass('progress-bar-striped');
+                $('#loading-progress-bar div span').html('Well done! ~\(≧▽≦)/~');
+                $('#loading-progress-bar').delay(4000).fadeOut(1000, function() {$(this).remove()});
+            },
+            function (err) {
+                $('#loading-progress-bar div').removeClass('progress-bar-striped').removeClass('progress-bar-blue').addClass('progress-bar-danger');
+                $('#loading-progress-bar div span').html('Unable to load component: ' + visualize_file + '.js');
+                require(
+                    ['default'],
+                    function (default) {
+                    },
+                    function (err) {
+                    }
+                );
+            }
+        );
+    },
+    function (err) {
+        $('#loading-progress-bar div').removeClass('progress-bar-striped').removeClass('progress-bar-blue').addClass('progress-bar-danger');
+        $('#loading-progress-bar div span').html('Unable to load component: ' + data_file + '.js');
+        require(
+            ['default'],
+            function (default) {
+            },
+            function (err) {
+            }
+        );
+    }
+);
