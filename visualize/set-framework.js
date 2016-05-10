4cdcfa39cce171d9fc281c0407d42d5837ebd73b
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-05-11 01:12:28 +0800

    Update

--- visualize/set-framework.js
+++ visualize/set-framework.js
@@ -1,3 +1,4 @@
 // set-framework.js
 
 document.title = frame_data.general.basics.name + ' · Y-English GRE Test Preparation Framework';
+$('#site-version').html(footer_data.site.version + ' for ' + frame_data.general.basics.name);
