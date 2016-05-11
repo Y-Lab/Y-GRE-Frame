80e98a23a5c780f5ad36583d95ea56eeaa81df7f
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-05-12 02:47:46 +0800

    Update

--- visualize/data/footer-data.js
+++ visualize/data/footer-data.js
@@ -11,7 +11,7 @@ footer_data = {
       "prefix" : "Beijing ",
       "name" : "Y-English",
       "link" : "http:\/\/www.y-english.org\/",
-      "suffix" : " Education Co., Ltd."
+      "suffix" : " Education Technology Co., Ltd."
     },
   },
   "powered_by" : [
